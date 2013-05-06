9f59b9a3fde29becfe82fe951d208e9d4844ca61
TJ Holowaychuk <tj@vision-media.ca>
2013-05-06 09:14:57 -0700

    docs

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ var toBlob = require('data-uri-to-blob');
 var jpeg = require('jpeg-size');
 var png = require('png-size');
 
+/**
+ * Return the dimensions of data `uri`.
+ *
+ * @param {String} uri
+ * @return {Object}
+ * @api public
+ */
+
 module.exports = function(uri){
   var blob = toBlob(uri);
   return 'image/jpeg' == blob.type
